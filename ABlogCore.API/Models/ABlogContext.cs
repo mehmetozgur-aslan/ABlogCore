76883163c87b66b6fbb5ee3b4a45040551c85d27
Asus <Asus@DESKTOP-7QSBH7L>
2021-04-18 15:29:41 +0300

    Controllerlar oluşturuldu ve startup dosyası configure edildi.

--- ABlogCore.API/Models/ABlogContext.cs
+++ ABlogCore.API/Models/ABlogContext.cs
@@ -21,15 +21,6 @@ namespace ABlogCore.API.Models
         public virtual DbSet<Category> Categories { get; set; }
         public virtual DbSet<Comment> Comments { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-7QSBH7L;Initial Catalog=ABlog;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "Turkish_CI_AS");
--- ABlogCore.API/Startup.cs
+++ ABlogCore.API/Startup.cs
@@ -1,6 +1,8 @@
+using ABlogCore.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,18 @@ namespace ABlogCore.API
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddCors(opt =>
+            {
+                opt.AddDefaultPolicy(x => { x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials(); });
+            });
+
+            services.AddDbContext<ABlogContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration["ConnectionString:DefaultSqlConnectionString"]);
+
+
+            });
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -43,8 +57,9 @@ namespace ABlogCore.API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ABlogCore.API v1"));
             }
 
-            app.UseRouting();
-
+            app.UseRouting();           
+            app.UseCors();
+            app.UseStaticFiles();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
